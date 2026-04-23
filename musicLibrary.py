939e1bb6e2311167e30aab8a8c7a9932f9f65160
pythondeveloper483 <pythondeveloper483@gmail.com>
2026-04-23 07:56:21 +0545

    Create musicLibrary.py

---
+++ musicLibrary.py
@@ -0,0 +1,9 @@
+music = {
+    "shape of you": "https://youtube.com/watch?v=JGwWNGJdvx8",
+    "believer": "https://youtube.com/watch?v=7wtfhZwyrcc",
+    "my favourite song": "https://youtu.be/2jmBEm3nMa4?si=pCuiklpFfb_OSEXB",
+    "hindi song": "https://youtu.be/-lDmXk8pBNI?list=RD-lDmXk8pBNI",
+    "compulsion": "https://youtu.be/44Rlm7SGFPk?list=RD44Rlm7SGFPk",
+    "old version": "https://youtu.be/VYslt8bc-4Q?list=RDVYslt8bc-4Q",
+    "new version": "https://youtu.be/8Z-T5h_yJAI?si=q8k-sdy4BO_v8WiO"
+}
